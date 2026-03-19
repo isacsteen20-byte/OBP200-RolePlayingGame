0395c14eb16f6289deee2cb686b21342e8f28d5c
IsacSten <isacsteen20@gmail.com>
2026-03-19 17:14:28 +0100

    Character klass

---
+++ OBP200-RolePlayingGame/Character.cs
@@ -0,0 +1,29 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public class Character
+{
+    public string Name { get; set; }
+    public int HP { get; set; }
+    public int MaxHP { get; set; }
+    public int Attack { get; set; }
+    public int Defence { get; set; }
+
+    public bool IsDead()
+    {
+        return HP <= 0;
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        HP -= Math.Max(0, dmg);
+        if (HP < 0) HP = 0;
+    }
+    public Character(string name, int hp, int maxHp, int attack, int defence)
+    {
+        Name = name;
+        HP = hp;
+        MaxHP = maxHp;
+        Attack = attack;
+        Defence = defence;
+    }
+}
